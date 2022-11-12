82a350a4fb742466fb59146a8749b6ff495bac24
yashas666999 <108930241+yashas666999@users.noreply.github.com>
2022-11-12 10:06:26 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var canvas, backgroundImage;
+
+var questions;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+//call the start() method inside the quiz class
+
+}
+
+
+function draw(){
+  background("pink");
+  text("Question 1");
+  textSize(25);
+  text(fill="black");
+
+}
